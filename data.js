f90c4d9028bc1f125c27294424e28d71b05db4a7
Miikuu <fitrailmi144@gmail.com>
2026-04-24 11:12:07 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,20 @@
 const databaseProyek = {
-    "": {
-        judul: "",
-        keahlian: [],
-        gambar: "",
-        latar: "",
-        tantangan: "",
-        pelajaran: "",
+    "sistem_pos": {
+        judul: "SISTEM POS UNTUK PT Gitaris",
+        keahlian: ["Menggenjreng gitar/memetik gitar"],
+        gambar: "https://jogja.disway.id/upload/6d351b760798b94f7e795f391ba70004.jpg",
+        latar: "ini adalah proyek magang dimana saya membuat sistem pos untuk PT.Gitaris Indonesia",
+        tantangan: "Sejauh ini belum ada",
+        pelajaran: "Saya mendapatkan pelajaran bahwa sistem pos PT Gitaris ideal untuk skala kecil",
+        link: ""
+    },
+    "sistem_pos_xyz": {
+        judul: "PT Gitaris indonesia",
+        keahlian: ["Menggenjreng gitar/memetik gitar"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP5Pg9N5_l2NSJCeqNFgUmdIpBixHrCy5QMA&s",
+        latar: "1.799.000",
+        tantangan: "Gitar Listrik",
+        pelajaran: "kenyamanan bermain,keserbaungan suara,kontrol volume,estetika dan variasi",
         link: ""
     },
 };
